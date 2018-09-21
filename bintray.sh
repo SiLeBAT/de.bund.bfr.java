4a6de043fe5dbf7df88a5e2b06409c312a4556c9
Miguel <miguel.de-alba-aparicio@bfr.bund.de>
2018-09-21 13:53:04 +0200

    Remove features from deployment script

--- bintray.sh
+++ bintray.sh
@@ -5,12 +5,6 @@ FOLDER="$TRAVIS_BUILD_DIR/releng/de.bund.bfr.java.update/target/repository"
 curl -u $BINTRAY_USER:$BINTRAY_KEY -T $FOLDER/artifacts.jar $API/artifacts.jar$PROPS
 curl -u $BINTRAY_USER:$BINTRAY_KEY -T $FOLDER/content.jar $API/content.jar$PROPS
 
-# Upload features (feature includes the features/ folder)
-for feature in $FOLDER/features/*.jar; do
-	file=$(basename -- "$feature")
-	curl -u $BINTRAY_USER:$BINTRAY_KEY -T $feature $API/features/$file$PROPS
-done
-
 # Upload plugins (plugin includes the plugins/ folder)
 for plugin in $FOLDER/plugins/*.jar; do
 	file=$(basename -- "$plugin")
